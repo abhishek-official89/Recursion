f9c98e98a8946b0dc94e2a8dbb69d3f1ca0e6f9d
abhishek-official89 <70819479+abhishek-official89@users.noreply.github.com>
2020-09-07 00:33:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display(0);
+            Console.ReadLine();
+
+
+        }
+
+
+        public static void Display(int count)
+        {
+           if(count<10)
+
+           {
+                Display(count + 1);
+                Console.WriteLine(count);
+               
+               
+               
+            
+            
+            }
+        }
+    }
+}
